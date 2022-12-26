69bcb1a9a7f4e08c0cd6cc3f746b8939cc8cd940
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-12-26 21:49:04 +0300

    Safe Point #2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  {
+    return (
+      <View>
+        <Text className="text-red-500"> textInComponent </Text>
+      </View>
+    );
+  }
+};
+
+export default HomeScreen;
